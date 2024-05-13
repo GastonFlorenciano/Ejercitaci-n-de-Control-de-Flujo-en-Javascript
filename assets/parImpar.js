764a51d12fe7afef45314d5747de0d1968771054
GastonFlorenciano <gaston__florenciano@hotmail.com>
2024-05-13 10:52:12 -0300

    FINALIZACION DEL COMPONENTE PARIMPAR.JS

---
+++ assets/parImpar.js
@@ -0,0 +1,9 @@
+let numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] % 2 === 0){
+        console.log('ES PAR');
+    }else{
+        console.log('ES IMPAR')
+    }
+}
